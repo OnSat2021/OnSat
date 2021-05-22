78b286fd02eb196147314f0242364ab7369e68c7
Filippo Carboni <56604932+filopixel@users.noreply.github.com>
2021-05-22 14:55:39 +0200

    dati su mac

---
+++ www/components/signin_components.js
@@ -0,0 +1,7 @@
+Vue.component('sign-in', {
+    template: `
+    <div class="relative top-0 left-0 h-full w-screen bg-dark text-white text-center font-bold flex flex-col justify-start">
+        <section-header title="Accedi" subtitle="Scopri i nostri prodotti"></section-header>
+    </div>
+    `
+});
